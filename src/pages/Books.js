f371eb550dc3b40208836b6e5bceca554bf1e857
sgollapudi77 <sgollapudi@microsoft.com>
2021-09-10 20:34:10 +0530

    Changed function to Class using React Component

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -1,11 +1,31 @@
 import React from 'react';
 
-function Books(){
-    return(
-        <>
-        <h1>Welcome to Books</h1>
-        </>
-    );
+class Books extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {data:""};
+    }
+
+    callAPI() {
+        fetch("http://localhost:7071/api/test")
+            .then(res => res.text())
+            .then(res => this.setState({ data: res }));
+    }
+    
+    componentWillMount() {
+        this.callAPI();
+        console.log(this.state.data);
+    }
+
+    render(){
+        this.callAPI();
+        return(
+            <>
+            <h1>Welcome to Books</h1>
+            <p>Data from API :{this.state.data}</p>
+            </>
+        );
+    }
 }
 
 export default Books;
