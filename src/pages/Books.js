6458b999863b6f13918ebede86ed433025d40e4f
sgollapudi77 <sgollapudi@microsoft.com>
2021-09-12 12:51:24 +0530

    Updated Cryptopage

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -6,18 +6,10 @@ class Books extends React.Component{
         this.state = {data:""};
     }
 
-    callAPI() {
-        fetch("http://localhost:7071/api/crypto")
-            .then(res => res.text())
-            .then(res => this.setState({ data: res }));
-    }
-
     render(){
-        this.callAPI();
         return(
             <>
             <h1>Welcome to Books</h1>
-            <p>Data from API :{this.state.data}</p>
             </>
         );
     }
--- src/pages/CryptoPortfolio.js
+++ src/pages/CryptoPortfolio.js
@@ -1,11 +1,26 @@
 import React from 'react';
 
-function CryptoPortfolio(){
-    return(
-        <>
-        <h1>Welcome to Crypto Portfolio</h1>
-        </>
-    );
+class CryptoPortfolio extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {data:""};
+    }
+
+    callAPI() {
+        fetch("/api/crypto")
+            .then(res => res.text())
+            .then(res => this.setState({ data: res }));
+    }
+
+    render(){
+        this.callAPI();
+        return(
+            <>
+            <h1>Welcome to Crypto</h1>
+            <p>Data from API :{this.state.data}</p>
+            </>
+        );
+    }
 }
 
 export default CryptoPortfolio;
