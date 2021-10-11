8f3123d5eff6117364b5f8584e0fb6a72aa965dd
sgollapudi77 <sgollapudi@microsoft.com>
2021-10-11 10:44:31 +0530

    Added stock api

--- src/pages/StocksAlgo.js
+++ src/pages/StocksAlgo.js
@@ -1,11 +1,26 @@
 import React from 'react';
 
-function StocksAlgo(){
-    return(
-        <>
-        <h1>Welcome to StocksAlgo</h1>
-        </>
-    );
+class StocksAlgo extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {data:""};
+    }
+
+    callAPI() {
+        fetch("/api/stocks")
+            .then(res => res.text())
+            .then(res => this.setState({ data: res }));
+    }
+
+    render(){
+        this.callAPI();
+        return(
+            <>
+            <h1>Welcome to Stocks data</h1>
+            <p>Data from API :{this.state.data}</p>
+            </>
+        );
+    }
 }
 
 export default StocksAlgo;
