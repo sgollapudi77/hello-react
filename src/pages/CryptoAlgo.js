95f7a984fdffbe51aa5d8029a8eb9c003f23f125
sgollapudi77 <sgollapudi@microsoft.com>
2021-10-12 17:25:57 +0530

    feat: Added football page feat: Added input option to get current price of a stock

--- src/pages/CryptoAlgo.js
+++ src/pages/CryptoAlgo.js
@@ -1,11 +1,26 @@
 import React from 'react';
 
-function CryptoAlgo(){
-    return(
-        <>
-        <h1>Welcome to CryptoAlgo</h1>
-        </>
-    );
+class CryptoAlgo extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {data:""};
+    }
+
+    callAPI() {
+        fetch("/api/crypto")
+            .then(res => res.text())
+            .then(res => this.setState({ data: res }));
+    }
+
+    render(){
+        this.callAPI();
+        return(
+            <>
+            <h1>Welcome to Crypto</h1>
+            <p>Data from API :{this.state.data}</p>
+            </>
+        );
+    }
 }
 
 export default CryptoAlgo;
--- src/pages/CryptoPortfolio.js
+++ src/pages/CryptoPortfolio.js
@@ -6,18 +6,11 @@ class CryptoPortfolio extends React.Component{
         this.state = {data:""};
     }
 
-    callAPI() {
-        fetch("/api/crypto")
-            .then(res => res.text())
-            .then(res => this.setState({ data: res }));
-    }
-
     render(){
         this.callAPI();
         return(
             <>
             <h1>Welcome to Crypto</h1>
-            <p>Data from API :{this.state.data}</p>
             </>
         );
     }
