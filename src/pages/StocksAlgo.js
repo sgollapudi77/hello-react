d5ed3d3c992791cd597ba97dbd908d07e3e0ea31
sgollapudi77 <sgollapudi@microsoft.com>
2021-10-12 18:31:39 +0530

    Added form for stock input

--- src/pages/StocksAlgo.js
+++ src/pages/StocksAlgo.js
@@ -3,11 +3,28 @@ import React from 'react';
 class StocksAlgo extends React.Component{
     constructor(props){
         super(props);
-        this.state = {data:""};
+        this.state = {
+            data:"",
+            value:""
+        };
+        this.sendData = this.sendData.bind(this);
+        this.takeInput = this.takeInput.bind(this);
+    }
+
+    takeInput(event){
+        this.setState({value:event.target.value})
+    }
+
+    sendData(event){
+        // alert('Submitted '+this.state.value)
+        this.callAPI()
+        event.preventDefault();
     }
 
     callAPI() {
-        fetch("/api/stock?name=MSFT")
+    let url = "/api/stock?name="
+        url += this.state.value
+        fetch(url)
             .then(res => res.text())
             .then(res => this.setState({ data: res }));
     }
@@ -17,7 +34,13 @@ class StocksAlgo extends React.Component{
         return(
             <>
             <h1>Welcome to Stocks data</h1>
-            <p>Data from API :{this.state.data}</p>
+            <form onSubmit = {this.sendData}>
+                <label>Enter stock</label>
+                <br/>
+                <input type="text" value={this.state.value} onChange={this.takeInput}/>
+            </form>
+            <p>Current price of {this.state.value} is {this.state.data}</p>
+            <p>New functionality which allows user to input the time frame is also in progress</p>
             </>
         );
     }
